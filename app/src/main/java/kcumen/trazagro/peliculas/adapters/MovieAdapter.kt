ace3322e2b7bc4a18b9c2d0877471c4812ce79c1
Dario Chamorro <dario.chamorro.v@gmail.com>
2017-12-06 21:07:26 -0500

    Adapter implemented

---
+++ app/src/main/java/kcumen/trazagro/peliculas/adapters/MovieAdapter.kt
@@ -0,0 +1,47 @@
+package kcumen.trazagro.peliculas.adapters
+
+
+import android.content.Context
+import android.net.Uri
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.squareup.picasso.Picasso
+import kcumen.trazagro.peliculas.R
+import kcumen.trazagro.peliculas.model.Movie
+import kcumen.trazagro.peliculas.util.inflate
+import kotlinx.android.synthetic.main.template_movie.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MovieAdapter:RecyclerView.Adapter<MovieHolder>(){
+
+    var data:List<Movie> = emptyList()
+
+    override fun onBindViewHolder(holder: MovieHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieHolder =
+            MovieHolder(parent.inflate(R.layout.template_movie))
+
+    override fun getItemCount(): Int = data.size
+
+}
+
+class MovieHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    val dateFormat:SimpleDateFormat = SimpleDateFormat("yyyy/MM/dd",Locale.getDefault())
+    val context: Context = view.context
+
+    fun bind(movie: Movie){
+        itemView.name.text = movie.name
+        itemView.duration.text = context.getString(R.string.duration, movie.duration)
+        itemView.release.text = dateFormat.format(movie.releaseDate)
+        Picasso.with(context)
+                .load(Uri.parse(movie.img))
+                .into(itemView.img)
+
+    }
+}
---
+++ app/src/main/java/kcumen/trazagro/peliculas/model/Movie.kt
@@ -0,0 +1,12 @@
+package kcumen.trazagro.peliculas.model
+
+import java.util.*
+
+/**
+ * Created by darfe on 6/12/2017.
+ */
+class Movie(val img:String,
+            val name:String,
+            val duration:Int,
+            val releaseDate: Date,
+            val sinopsis:String)
